49a2b87c00974b65ac031e132f7a6bf41db291e3
hem_b <hemanthb93@gmail.com>
2022-05-19 16:10:24 +0530

    [Hemanth]:Check whther a number is perfect number or not

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,15 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Enter your Choice to execute required following Classes");
 int choice = Convert.ToInt32(Console.ReadLine());
-
+int number;
 switch(choice)
 {
-    case 0:
+    case 1:
         LogicalPrograms.FibonacciSeriesUc1 fibonacciSeries = new LogicalPrograms.FibonacciSeriesUc1();
         Console.WriteLine("Enter a number till which fibonacci series has to be printed");
-        int number = Convert.ToInt32(Console.ReadLine());
+        number = Convert.ToInt32(Console.ReadLine());
         fibonacciSeries.PrintFibonacciSeries(number);
         Console.WriteLine();
+        break;
+    case 2:
+        LogicalPrograms.PerfectNumberUc2 perfectNumber =  new LogicalPrograms.PerfectNumberUc2();
+        Console.WriteLine("Enter a number whther its Perfect number or not");
+        number = Convert.ToInt32(Console.ReadLine());
+        perfectNumber.CheckPerfectNumber(number);
         break;
     default:
         break;
