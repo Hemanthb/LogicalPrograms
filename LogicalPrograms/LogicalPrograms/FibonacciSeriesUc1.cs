7e89bbe83ba8b4b78717b0acb04866572574ae61
hem_b <hemanthb93@gmail.com>
2022-05-19 15:52:42 +0530

    [Hemanth]:Fibonacci Series is printed

---
+++ LogicalPrograms/LogicalPrograms/FibonacciSeriesUc1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeriesUc1
+    {
+        int c,a = 0,b = 1;
+        public void PrintFibonacciSeries(int num)
+        {
+            Console.WriteLine("Fibonacci Series is -");
+            Console.Write(a + " " + b + " ");
+            for(int i = 0; i < num; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.Write(c+" ");
+            }
+        }
+        
+
+    }
+}
---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your Choice to execute required following Classes");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+switch(choice)
+{
+    case 0:
+        LogicalPrograms.FibonacciSeriesUc1 fibonacciSeries = new LogicalPrograms.FibonacciSeriesUc1();
+        Console.WriteLine("Enter a number till which fibonacci series has to be printed");
+        int number = Convert.ToInt32(Console.ReadLine());
+        fibonacciSeries.PrintFibonacciSeries(number);
+        Console.WriteLine();
+        break;
+    default:
+        break;
+}
